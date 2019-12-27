516d6aca9718bc097f68d12f7eb1daa5be218496
devbaj <bjory7@gmail.com>
2019-12-27 00:16:45 -0800

    initial commit, hello world added

---
+++ basics/helloworld.c
@@ -0,0 +1,26 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  helloworld.c
+ *
+ *    Description:  my first c program
+ *
+ *        Version:  1.0
+ *        Created:  12/27/2019 12:10:45 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+    printf("hello world!\n");
+
+    return 0;
+}
